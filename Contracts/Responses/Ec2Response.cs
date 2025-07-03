bda987e17c8c793f10ba6489c0e2f719b0a6c7bd
Agustina Fassina | ENG <agustinafassina@gmail.com>
2025-07-03 09:39:39 -0300

    new classes for the responses

---
+++ Contracts/Responses/Ec2Response.cs
@@ -0,0 +1,8 @@
+namespace AwsApi.Contracts.Responses
+{
+    public class Ec2Response
+    {
+        public string InstanceId { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Contracts/Responses/RdsResponse.cs
@@ -0,0 +1,8 @@
+namespace AwsApi.Contracts.Responses
+{
+    public class RdsResponse
+    {
+        public string Id { get; set; }
+        public string Status { get; set; }
+    }
+}
